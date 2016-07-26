a00bc843b469fbdd6d18f18f52398d2e9c3c3fa6
KarlJoones <karljones@live.ie>
2016-07-26 18:34:32 -0400

    Added MAC address

--- app/src/main/java/development/iarratais/fragment/NetworkFragment.java
+++ app/src/main/java/development/iarratais/fragment/NetworkFragment.java
@@ -8,15 +8,7 @@ package development.iarratais.fragment;
 
 
 import android.app.Fragment;
-import android.bluetooth.BluetoothAdapter;
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.net.wifi.SupplicantState;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.text.format.Formatter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -108,7 +100,7 @@ public class NetworkFragment extends Fragment {
         TextView wiFiStatusTextView = (TextView) rootView.findViewById(R.id
                 .textview_network_information_wifi_status);
 
-        if (isWiFiEnabled()){
+        if (networkInfoUtil.isWiFiEnabled()){
             String enabled = getString(R.string.enabled);
             wiFiStatusTextView.setText(getString(R.string.network_information_wifi_status, enabled));
         } else {
@@ -120,8 +112,9 @@ public class NetworkFragment extends Fragment {
         TextView SSIDTextView = (TextView) rootView.findViewById(R.id
                 .textView_network_information_wifi_SSID);
 
-        if(isWiFiEnabled()){
-            String SSIDtext = getString(R.string.network_information_wifi_SSID, getSSID());
+        if(networkInfoUtil.isWiFiEnabled()){
+            String SSIDtext = getString(R.string.network_information_wifi_SSID, networkInfoUtil
+                    .getSSID());
             SSIDTextView.setText(SSIDtext);
             SSIDTextView.setVisibility(View.VISIBLE);
         } else {
@@ -132,14 +125,23 @@ public class NetworkFragment extends Fragment {
         TextView IPAdressTextView = (TextView) rootView.findViewById(R.id
                 .textview_network_information_ip_address);
 
-        String ipadresstext = getString(R.string.network_information_ip_address, getIPAddress());
+        String ipadresstext = getString(R.string.network_information_ip_address, networkInfoUtil
+                .getIPAddress());
         IPAdressTextView.setText(ipadresstext);
+
+        // Set the MAC Address.
+        TextView MACAddressTextView = (TextView) rootView.findViewById(R.id
+                .textview_network_information_mac_address);
+
+        String macAddressText = getString(R.string.network_information_mac_address,
+                networkInfoUtil.getMACAddress());
+        MACAddressTextView.setText(macAddressText);
 
         // Set the textview for mobile status
         TextView mobileDataStatusTextView = (TextView) rootView.findViewById(R.id
                 .textview_network_information_mobile_data_status);
-        if(deviceHasMobileData()) {
-            if (isMobileDataEnabled()) {
+        if(networkInfoUtil.deviceHasMobileData()) {
+            if (networkInfoUtil.isMobileDataEnabled()) {
                 String enabled = getString(R.string.enabled);
                 mobileDataStatusTextView.setText(getString(R.string
                         .network_information_mobile_data_status, enabled));
@@ -158,7 +160,7 @@ public class NetworkFragment extends Fragment {
         TextView bluetoothStatusTextView = (TextView) rootView.findViewById(R.id
                 .textview_network_information_bluetooth_status);
 
-        if(isBluetoothAvailable()){
+        if(networkInfoUtil.isBluetoothAvailable()){
             String enabled = getString(R.string.enabled);
             bluetoothStatusTextView.setText(getString(R.string
                         .network_information_bluetooth_status, enabled));
@@ -170,70 +172,5 @@ public class NetworkFragment extends Fragment {
 
     }
 
-    /**
-     * Check if the WiFi is enabled.
-     *
-     * @return true if enabled.
-     */
-    private boolean isWiFiEnabled(){
-        ConnectivityManager conMan = (ConnectivityManager) getActivity().getSystemService(Context
-                .CONNECTIVITY_SERVICE);
 
-        NetworkInfo.State wifi = conMan.getNetworkInfo(1).getState();
-
-        return wifi == NetworkInfo.State.CONNECTED || wifi == NetworkInfo.State.CONNECTING;
-    }
-
-    /**
-     * Check if the mobile data is enabled.
-     *
-     * @return true if enabled.
-     */
-    private boolean isMobileDataEnabled() {
-        ConnectivityManager conMan = (ConnectivityManager) getActivity().getSystemService(Context
-                .CONNECTIVITY_SERVICE);
-
-        NetworkInfo.State mobile;
-
-        if(deviceHasMobileData()) {
-            mobile = conMan.getNetworkInfo(0).getState();
-            return mobile == NetworkInfo.State.CONNECTED || mobile == NetworkInfo.State.CONNECTING;
-        }
-
-        return false;
-    }
-
-    private boolean deviceHasMobileData(){
-        ConnectivityManager conMan = (ConnectivityManager) getActivity().getSystemService(Context
-                .CONNECTIVITY_SERVICE);
-        NetworkInfo ni = conMan.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-
-        return ni != null;
-    }
-
-    private String getSSID(){
-        WifiManager wifiManager = (WifiManager)getActivity().getSystemService(Context.WIFI_SERVICE);
-        WifiInfo wifiInfo;
-
-        wifiInfo = wifiManager.getConnectionInfo();
-        if (wifiInfo.getSupplicantState()== SupplicantState.COMPLETED) {
-            return wifiInfo.getSSID();
-        }
-        return "None";
-    }
-
-    private String getIPAddress(){
-        WifiManager wm = (WifiManager) getActivity().getSystemService(Context.WIFI_SERVICE);
-        return Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
-    }
-
-    /**
-     * Check for Bluetooth.
-     *
-     * @return True if Bluetooth is available.
-     */
-    public static boolean isBluetoothAvailable() {
-        final BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        return (bluetoothAdapter != null && bluetoothAdapter.isEnabled());
-    }
 }
--- app/src/main/java/development/iarratais/utils/NetworkInfoUtil.java
+++ app/src/main/java/development/iarratais/utils/NetworkInfoUtil.java
@@ -6,7 +6,14 @@
 
 package development.iarratais.utils;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.SupplicantState;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.text.format.Formatter;
 
 /**
  * Created by KarlJones on 24/07/16.
@@ -21,5 +28,76 @@ public class NetworkInfoUtil {
         this.context = context;
     }
 
+    /**
+     * Check if the WiFi is enabled.
+     *
+     * @return true if enabled.
+     */
+    public boolean isWiFiEnabled(){
+        ConnectivityManager conMan = (ConnectivityManager) context.getSystemService
+                (Context
+                .CONNECTIVITY_SERVICE);
 
+        NetworkInfo.State wifi = conMan.getNetworkInfo(1).getState();
+
+        return wifi == NetworkInfo.State.CONNECTED || wifi == NetworkInfo.State.CONNECTING;
+    }
+
+    /**
+     * Check if the mobile data is enabled.
+     *
+     * @return true if enabled.
+     */
+    public boolean isMobileDataEnabled() {
+        ConnectivityManager conMan = (ConnectivityManager) context.getSystemService(Context
+                .CONNECTIVITY_SERVICE);
+
+        NetworkInfo.State mobile;
+
+        if(deviceHasMobileData()) {
+            mobile = conMan.getNetworkInfo(0).getState();
+            return mobile == NetworkInfo.State.CONNECTED || mobile == NetworkInfo.State.CONNECTING;
+        }
+
+        return false;
+    }
+
+    public boolean deviceHasMobileData(){
+        ConnectivityManager conMan = (ConnectivityManager) context.getSystemService(Context
+                .CONNECTIVITY_SERVICE);
+        NetworkInfo ni = conMan.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+        return ni != null;
+    }
+
+    public String getSSID(){
+        WifiManager wifiManager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo;
+
+        wifiInfo = wifiManager.getConnectionInfo();
+        if (wifiInfo.getSupplicantState()== SupplicantState.COMPLETED) {
+            return wifiInfo.getSSID();
+        }
+        return "None";
+    }
+
+    public String getIPAddress(){
+        WifiManager wm = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        return Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+    }
+
+    public String getMACAddress(){
+        WifiManager manager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        return manager.getConnectionInfo().getMacAddress();
+    }
+
+    /**
+     * Check for Bluetooth.
+     *
+     * @return True if Bluetooth is available.
+     */
+    public boolean isBluetoothAvailable() {
+        final BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        return (bluetoothAdapter != null && bluetoothAdapter.isEnabled());
+    }
 }
